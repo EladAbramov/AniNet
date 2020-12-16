efee59c070cae9e17300d820b0159e9babf0619d
eladabramov1989 <eladsoftware20@gmail.com>
2020-12-16 22:04:53 +0200

    server and client

--- server/server.js
+++ server/server.js
@@ -7,15 +7,33 @@ const port = 5000;
 
 // Function to connect to mongodb
 async function connectDB(){
-    await mongoose.connect("mongodb+srv://auth1:NIaDEoqcSjw4t2on@cluster0.fbeoz.mongodb.net/aninet", { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(
+        "mongodb+srv://auth1:NIaDEoqcSjw4t2on@cluster0.fbeoz.mongodb.net/aninet",
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }
+    );
     console.log("Successfully connected to database");
 }
 
 connectDB();
 
-// Model
-const schema = new mongoose.Schema({ email: 'string', password: 'string' });
+// Regular user Model
+const schema = new mongoose.Schema({
+    ownerName: 'string',
+    animalName: 'string',
+    animalType: 'string',
+    videoUrl: 'string',
+    email: 'string',
+    password: 'string'
+});
 const User = mongoose.model('User', schema);
+
+
+// Vet Model
+const vetSchema = new mongoose.Schema({ name: 'string', email: 'string', password: 'string' });
+const Vet = mongoose.model('Vet', vetSchema);
 
 // This take the post body
 app.use(express.json({ extended: false }));
@@ -25,14 +43,49 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 });
 
-// SignUp route
+
+// Sign Up Vet route
+app.post('/signupvet', async (req, res) => {
+    const { name, email, password } = req.body;
+    let vetCheck = await Vet.findOne({email});
+    if(vetCheck) {
+        return res.json({msg: "This email is already taken"});
+    }
+    let vet = new Vet({
+        name,
+        email,
+        password
+    });
+    await vet.save();
+    var vetToken = jwt.sign({ id: vet.id }, "pass");
+    res.json({vetToken: vetToken});
+});
+// Login Vet route
+app.post('/loginvet', async (req, res) => {
+    const { name, email, password } = req.body;
+    let vet = await Vet.findOne({email});
+    if(!vet) {
+        return res.json({msg: "No vet found with that email"});
+    }
+    if(vet.password !== password) {
+        return res.json({msg: "Password is not correct"});
+    }
+    var vetToken = jwt.sign({ id: vet.id }, "pass");
+    return res.json({vetToken: vetToken});
+});
+
+// Sign Up User route
 app.post('/signup', async (req, res) => {
-    const { email, password } = req.body;
+    const { ownerName, animalName, animalType, videoUrl, email, password } = req.body;
     let userCheck = await User.findOne({email});
     if(userCheck) {
         return res.json({msg: "This email is already taken"});
     }
     let user = new User({
+        ownerName,
+        animalName,
+        animalType,
+        videoUrl,
         email,
         password
     });
@@ -41,7 +94,8 @@ app.post('/signup', async (req, res) => {
     res.json({token: token});
 });
 
-// Login route
+
+// Login  User route
 app.post('/login', async (req, res) => {
     const { email, password } = req.body;
     let user = await User.findOne({email});
@@ -53,16 +107,6 @@ app.post('/login', async (req, res) => {
     }
     var token = jwt.sign({ id: user.id }, 'password');
     return res.json({token: token});
-});
-
-// Private route
-app.get('/private', async (req, res) => {
-    let token = req.header("token");
-    if(!token){
-        return res.json({msg: "This is a private route"});
-    }
-    var decoded = jwt.verify(token, "password");
-    console.log(decoded.id);
 });
 
 app.listen(port, () => {
